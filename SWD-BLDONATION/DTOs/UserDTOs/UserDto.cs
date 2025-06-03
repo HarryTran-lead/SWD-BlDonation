6da92120a6d35292ae0c3d44f028b95a02f33e79
Harry <thcstrandainghiathinh6a1@gmail.com>
2025-06-03 15:37:59 +0700

    fix field user

--- SWD-BLDONATION/DTOs/UserDTOs/UserDto.cs
+++ SWD-BLDONATION/DTOs/UserDTOs/UserDto.cs
@@ -5,7 +5,7 @@ public class UserDto
     public int UserId { get; set; }
     public string? UserName { get; set; }
 
-    [JsonPropertyName("Full Name")]
+    [JsonPropertyName("Name")]
     public string Name { get; set; }
 
     public string? Email { get; set; }
