2e7bf202437fa0e3968e6d92f4641147c1129fae
Harry <thcstrandainghiathinh6a1@gmail.com>
2025-06-12 00:37:21 +0700

    Merge remote-tracking branch 'origin/main'

--- SWD-BLDONATION/Models/Generated/BloodDonationContext.cs
+++ SWD-BLDONATION/Models/Generated/BloodDonationContext.cs
@@ -39,7 +39,7 @@ public partial class BloodDonationContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=BloodDonation;User ID=sa;Password=sa;Encrypt=True;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=BloodDonation;User ID=ad;Password=123;Encrypt=True;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
